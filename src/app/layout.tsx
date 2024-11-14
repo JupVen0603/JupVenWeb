996398a2c55111d0895a6d0397e218581720ea8a
JupVen0603 <satyamsen167@gmail.com>
2024-11-14 15:35:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>JupVen WebTech</title>
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
